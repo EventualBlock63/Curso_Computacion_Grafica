ab017daea0a6476b6f99f2d663414e8fa477c0b1
EventualBlock63 <ferny200291@gmail.com>
2025-03-12 10:44:36 -0600

    Inicio de estacionamiento

--- ConfigInicial/Carga de Modelos.cpp
+++ ConfigInicial/Carga de Modelos.cpp
@@ -108,7 +108,7 @@ int main( )
     Model piston((char*)"Models/13476_Engine_Piston_v1_l1.obj");
     Model motor((char*)"Models/13474_V6_Engine_Block_v1_l2.obj");
     Model car((char*)"Models/Porsche_911_GT2_v2.obj");
-    Model alternador((char*)"Models/uploads_files_2405461_AG1.obj"); 
+    Model estacionamiento((char*)"Models/ParkingLot.obj"); 
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -138,7 +138,7 @@ int main( )
         glm::mat4 modelPiston(1);
         glm::mat4 modelMotor(1);
         glm::mat4 modelCarro(1);
-        glm::mat4 modelAlt(1);
+        glm::mat4 modelPark(1);
 
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         dog.Draw(shader);
@@ -167,11 +167,11 @@ int main( )
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelCarro));
         car.Draw(shader);
         //--------------------------------------------------------------------------------------------------
-        modelAlt = glm::translate(modelAlt, glm::vec3(-5.0f, 0.0f, 0.0f));
-        modelAlt = glm::scale(modelAlt, glm::vec3(0.025f, 0.025f, 0.025f));
-        //modelCarro = glm::rotate(modelCarro, 1.5f, glm::vec3(-1.0f, 0.0f, 0.0f));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelAlt));
-        alternador.Draw(shader);
+        modelPark = glm::translate(modelPark, glm::vec3(-15.0f, -0.7f, 0.0f));
+        modelPark = glm::scale(modelPark, glm::vec3(0.5f, 0.5f, 0.5f));
+        modelPark = glm::rotate(modelPark, 1.5f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(modelPark));
+        estacionamiento.Draw(shader);
 
         // Swap the buffers
         glfwSwapBuffers( window );
